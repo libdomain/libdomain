ef0ea6a4b3ce02502577c0d9ad61ec923a3e416f
august-alt <77973983+august-alt@users.noreply.github.com>
2023-03-10 15:58:14 +0400

    feat: add spellcheck during ci pipeline
    This commit adds ability to perform spell checking during ci workflow.
    We use codespell to check *.md files and our code to find typos.

---
+++ script/spellcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+set -x
+
+CODESPELL_SRC=${@:-".github/pull_request_template.md .github/ISSUE_TEMPLATE/bug_report.md .github/ISSUE_TEMPLATE/feature_request.md README.md src/ docs/source tests/"}
+
+# Using option -c to enable colors, even when not printing to terminal.
+# Using option -f to check file names.
+# Using option -i to disable interactivity.
+# Builtin option takes a list of dictionaries to use.
+codespell -c -f -i 0 --builtin clear,rare,informal,usage,code,names $CODESPELL_SRC
+
