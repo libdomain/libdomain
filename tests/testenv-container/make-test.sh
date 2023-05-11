e8aba4a6a4a2f3bcaad73d327d9a3d2bd688d2d0
august-alt <77973983+august-alt@users.noreply.github.com>
2023-05-11 18:32:07 +0400

    wip: add testenv container to run tests in it

---
+++ tests/testenv-container/make-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+
+BUILD_DIR=build
+
+chown -R builder2:builder2 /app/
+cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest
