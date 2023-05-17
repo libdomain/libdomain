4f237cd6fb749a942ecffcd4a4a0cb024772c34f
august-alt <77973983+august-alt@users.noreply.github.com>
2023-05-17 18:07:10 +0400

    fix: update test environment

--- tests/openldap-container/start-ldap.sh
+++ tests/openldap-container/start-ldap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Start slapd
-slapd -h "ldap://0.0.0.0:3890/" -f /tmp/slapd.conf
+slapd -h "ldap://0.0.0.0:3890/" -f /tmp/slapd.conf &
 
 # Without this line our container will fail to keep running
-tail -f /dev/null
+tail -f /dev/null &
 
