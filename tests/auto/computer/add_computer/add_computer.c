a622d30ab6f7bf182f2c81b72144207ad5032556
august-alt <77973983+august-alt@users.noreply.github.com>
2023-06-08 15:48:12 +0400

    fix: modify computer addition test

--- tests/auto/computer/add_computer/add_computer.c
+++ tests/auto/computer/add_computer/add_computer.c
@@ -15,9 +15,86 @@ Describe(Cgreen);
 BeforeEach(Cgreen) {}
 AfterEach(Cgreen) {}
 
+const int CONNECTION_UPDATE_INTERVAL = 1000;
+
+static void connection_on_add_message(verto_ctx *ctx, verto_ev *ev)
+{
+    (void)(ev);
+
+    static int callcount = 0;
+
+    if (++callcount > 10)
+    {
+        verto_break(ctx);
+    }
+}
+
+static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
+{
+    (void)(ctx);
+
+    struct ldap_connection_ctx_t* connection = verto_get_private(ev);
+
+    if (connection->state_machine->state == LDAP_CONNECTION_STATE_RUN)
+    {
+        verto_del(ev);
+
+        int rc = ld_add_computer(connection->handle, "test_computer_add", "description", "100", "dc=domain,dc=alt");
+        if (rc != RETURN_CODE_SUCCESS)
+        {
+            verto_break(ctx);
+
+            fail_test("Error encountered during bind\n");
+        }
+        else
+        {
+            ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
+        }
+    }
+
+    if (connection->state_machine->state == LDAP_CONNECTION_STATE_ERROR)
+    {
+        verto_break(ctx);
+
+        fail_test("Error encountered during bind\n");
+    }
+}
+
+static enum OperationReturnCode connection_on_error(int rc, void* unused_a, void* connection)
+{
+    (void)(unused_a);
+
+    assert_that(rc, is_not_equal_to(LDAP_SUCCESS));
+
+    verto_break(((ldap_connection_ctx_t*)connection)->base);
+
+    fail_test("Computer addition was not successful\n");
+
+    return RETURN_CODE_SUCCESS;
+}
+
 xEnsure(Cgreen, computer_add_test)
 {
-    fail_test("Computer addition was not successful\n");
+    TALLOC_CTX* talloc_ctx = talloc_new(NULL);
+
+    char *envvar = "LDAP_SERVER";
+    char *server = get_environment_variable(talloc_ctx, envvar);
+
+    config_t *config = ld_create_config(server, 0, LDAP_VERSION3, "dc=domain,dc=alt",
+                                        "admin", "password", true, false, true, false, CONNECTION_UPDATE_INTERVAL,
+                                        "", "", "");
+    LDHandle *handle = NULL;
+    ld_init(&handle, config);
+
+    ld_install_default_handlers(handle);
+    ld_install_handler(handle, connection_on_timeout, CONNECTION_UPDATE_INTERVAL);
+    ld_install_error_handler(handle, connection_on_error);
+
+    ld_exec(handle);
+
+    ld_free(handle);
+
+    talloc_free(talloc_ctx);
 }
 
 int main(int argc, char **argv) {
