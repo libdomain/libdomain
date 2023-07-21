0784fc410028d4320ea63e86d46347329d6db0fb
august-alt <77973983+august-alt@users.noreply.github.com>
2023-07-21 15:08:13 +0400

    feat: add startup script

---
+++ tests/samba-container/start-samba.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir /tmp/samba
+
+# Populating database
+i=0
+while [ $i -le 15 ]
+do
+    if ldapadd -x -f /tmp/domain.alt.ldif -H ldap://127.0.0.1:3890 -D "cn=admin,dc=domain,dc=alt" -w password ; then
+        break
+    else
+        sleep 2
+        i=$(( $i + 1 ))
+    fi
+done
+
+if [ $? -ne 0 ]; then
+    echo "Error while configuring slapd service!"
+    cat /tmp/slapd.log
+    exit 1
+fi
+
+# Without this line our container will fail to keep running
+tail -f /dev/null
