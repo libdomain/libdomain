a6229a192ded756bd5b0c216a45e70105cb1f29a
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-02 22:33:57 +0400

    feat: output diagnostic messages on errors

--- tests/testenv-container/make-test.sh
+++ tests/testenv-container/make-test.sh
@@ -3,4 +3,4 @@
 BUILD_DIR=build
 
 chown -R builder2:builder2 /app/
-cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest
+cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest --output-on-failure
