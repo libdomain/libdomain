0936f7de82e8e6e4c63e8dd2aacd0b72c7d85001
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-05 10:58:06 +0400

    chore: update password to support samba

--- tests/auto/add_entry/add_entry.c
+++ tests/auto/add_entry/add_entry.c
@@ -172,19 +172,20 @@ Ensure(Cgreen, entry_add_test) {
     struct context_t* ctx = create_context();
 
     ctx->config.use_sasl = true;
+    ctx->config.bind_type = BIND_TYPE_INTERACTIVE;
 
-    ctx->config.sasl_options = talloc(ctx->global_ctx.talloc_ctx, struct ldap_sasl_options_t);
-    ctx->config.sasl_options->mechanism = LDAP_SASL_SIMPLE;
-    ctx->config.sasl_options->passwd = "password";
+    ctx->config.sasl_options = talloc_zero(ctx->global_ctx.talloc_ctx, struct ldap_sasl_options_t);
+    ctx->config.sasl_options->mechanism = "GSSAPI";
+    ctx->config.sasl_options->passwd = "password145Qw!";
 
     ctx->config.sasl_options->sasl_nocanon = true;
-    ctx->config.sasl_options->sasl_secprops = "maxssf=56";
-    ctx->config.sasl_options->sasl_flags = LDAP_SASL_QUIET;
+    ctx->config.sasl_options->sasl_secprops = "maxssf=48";
+    ctx->config.sasl_options->sasl_flags = LDAP_SASL_INTERACTIVE;
     ctx->connection_ctx.ldap_params = talloc(ctx->global_ctx.talloc_ctx, struct ldap_sasl_params_t);
     ctx->connection_ctx.ldap_params->dn = "cn=admin,dc=domain,dc=alt";
     ctx->connection_ctx.ldap_params->passwd = talloc(ctx->global_ctx.talloc_ctx, struct berval);
-    ctx->connection_ctx.ldap_params->passwd->bv_len = strlen(ctx->config.sasl_options->passwd);
-    ctx->connection_ctx.ldap_params->passwd->bv_val = strdup(ctx->config.sasl_options->passwd);
+    ctx->connection_ctx.ldap_params->passwd->bv_len = strlen(ctx->config.sasl_options->passwd) - 1;
+    ctx->connection_ctx.ldap_params->passwd->bv_val = talloc_strdup(ctx->global_ctx.talloc_ctx, ctx->config.sasl_options->passwd);
     ctx->connection_ctx.ldap_params->clientctrls = NULL;
     ctx->connection_ctx.ldap_params->serverctrls = NULL;
 
