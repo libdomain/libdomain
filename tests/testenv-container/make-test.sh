8a8c53d0ecbd181e62190c2123663bbe62c801b8
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-05 10:54:59 +0400

    feat: add kinit to testenv container

--- tests/testenv-container/make-test.sh
+++ tests/testenv-container/make-test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
 
-BUILD_DIR=build
+export BUILD_DIR=build
+export KRB5CCNAME=`mktemp`
 
+mv /krb5.conf /etc/krb5.conf
+cat /resolv.conf > /etc/resolv.conf
 chown -R builder2:builder2 /app/
+echo 'password145Qw!' | kinit administrator@DOMAIN.ALT
 cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest --output-on-failure
