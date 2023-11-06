720b8be23ab3018c43dc1efb85415627d0869e2b
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-06 19:33:27 +0400

    fix: fix openldap environment

--- tests/testenv-container/make-test.sh
+++ tests/testenv-container/make-test.sh
@@ -6,5 +6,5 @@ export KRB5CCNAME=`mktemp`
 cp /krb5.conf /etc/krb5.conf
 cat /resolv.conf > /etc/resolv.conf
 chown -R builder2:builder2 /app/
-echo 'password145Qw!' | kinit administrator@DOMAIN.ALT
+echo 'password145Qw!' | kinit administrator@DOMAIN.ALT || :
 cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest --output-on-failure
