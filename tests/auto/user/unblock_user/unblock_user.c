d3293600a99d8b1f9b2adf076bd69d50e4ad9bf3
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-10 02:46:15 +0400

    feat: add unblock user test

--- tests/auto/user/unblock_user/unblock_user.c
+++ tests/auto/user/unblock_user/unblock_user.c
@@ -1,17 +1,94 @@
 #include <cgreen/cgreen.h>
 
 #include <domain.h>
+#include <user.h>
 #include <talloc.h>
 
+#include <connection_state_machine.h>
+
 #include <test_common.h>
+
+const int LDAP_DEBUG_ANY = -1;
+const int BUFFER_SIZE = 80;
 
 Describe(Cgreen);
 BeforeEach(Cgreen) {}
 AfterEach(Cgreen) {}
 
-xEnsure(Cgreen, unblock_user_test)
+const int CONNECTION_UPDATE_INTERVAL = 1000;
+
+static void connection_on_add_message(verto_ctx *ctx, verto_ev *ev)
 {
-    fail_test("Un block user test failed!");
+    (void)(ev);
+
+    static int callcount = 0;
+
+    if (++callcount > 10)
+    {
+        verto_break(ctx);
+    }
+}
+
+static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
+{
+    (void)(ctx);
+
+    struct ldap_connection_ctx_t* connection = verto_get_private(ev);
+
+    if (connection->state_machine->state == LDAP_CONNECTION_STATE_RUN)
+    {
+        verto_del(ev);
+
+        enum OperationReturnCode rc = ld_unblock_user(connection->handle, "test_block_user",
+                                                      NULL);
+        assert_that(rc, is_equal_to(RETURN_CODE_SUCCESS));
+
+        ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
+    }
+
+    if (connection->state_machine->state == LDAP_CONNECTION_STATE_ERROR)
+    {
+        verto_break(ctx);
+
+        fail_test("Error encountered during bind\n");
+    }
+}
+
+static enum OperationReturnCode connection_on_error(int rc, void* unused_a, void* connection)
+{
+    (void)(unused_a);
+
+    assert_that(rc, is_not_equal_to(LDAP_SUCCESS));
+
+    verto_break(((ldap_connection_ctx_t*)connection)->base);
+
+    fail_test("User block was not successful\n");
+
+    return RETURN_CODE_SUCCESS;
+}
+
+Ensure(Cgreen, user_unblock_test)
+{
+    TALLOC_CTX* talloc_ctx = talloc_new(NULL);
+
+    char *envvar = "LDAP_SERVER";
+    char *server = get_environment_variable(talloc_ctx, envvar);
+
+    config_t *config = ld_create_config(server, 0, LDAP_VERSION3, "dc=domain,dc=alt",
+                                        "admin", "password", true, false, true, false, CONNECTION_UPDATE_INTERVAL,
+                                        "", "", "");
+    LDHandle *handle = NULL;
+    ld_init(&handle, config);
+
+    ld_install_default_handlers(handle);
+    ld_install_handler(handle, connection_on_timeout, CONNECTION_UPDATE_INTERVAL);
+    ld_install_error_handler(handle, connection_on_error);
+
+    ld_exec(handle);
+
+    ld_free(handle);
+
+    talloc_free(talloc_ctx);
 }
 
 int main(int argc, char **argv) {
@@ -19,6 +96,6 @@ int main(int argc, char **argv) {
     (void)(argv);
     (void)(contextForCgreen);
     TestSuite *suite = create_test_suite();
-    add_test_with_context(suite, Cgreen, unblock_user_test);
+    add_test_with_context(suite, Cgreen, user_unblock_test);
     return run_test_suite(suite, create_text_reporter());
 }
