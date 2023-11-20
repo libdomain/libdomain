88387309c6c2836d42e618a4067e3d09b4c00b37
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-20 05:06:08 +0400

    feat: add sisyphus test environment (#38)
    * fix: fix add entry test
    
    * fix: fix error message in add entry test
    
    * fix: fix delete entry test
    
    * fix: fix delete entry test
    
    * fix: fix rename entry test
    
    * fix: fix modify entry test
    
    * feat: update dockerfile and domain.alt.ldif
    
    * feat: upate workflow
    
    * fix: fix dockerfile

--- tests/auto/delete_entry/delete_entry.c
+++ tests/auto/delete_entry/delete_entry.c
@@ -1,9 +1,10 @@
 #include <cgreen/cgreen.h>
 
-#include <connection.h>
-#include <connection_state_machine.h>
+#include <domain.h>
 #include <entry.h>
 #include <talloc.h>
+
+#include <connection_state_machine.h>
 
 #include <test_common.h>
 
@@ -14,94 +15,9 @@ Describe(Cgreen);
 BeforeEach(Cgreen) {}
 AfterEach(Cgreen) {}
 
-char* LDAP_DIRECTORY_ATTRS[] = { "objectClass", NULL };
-#define VALUE_ATTRIBUTES_SIZE 5
-
-typedef struct attribute_value_pair_s
-{
-    char* name;
-    char* value[VALUE_ATTRIBUTES_SIZE];
-} attribute_value_pair_t;
-
-#define number_of_elements(x)  (sizeof(x) / sizeof((x)[0]))
-
-attribute_value_pair_t LDAP_TEST_USER_ATTRIBUTES[] =
-{
-    { "objectClass", { "top", "account", "posixAccount", "shadowAccount" , NULL } },
-    { "cn", { "adam", NULL, NULL, NULL, NULL } },
-    { "uid", { "adam", NULL, NULL, NULL, NULL } },
-    { "uidNumber", { "0", NULL, NULL, NULL, NULL } },
-    { "gidNumber", { "0", NULL, NULL, NULL, NULL } },
-    { "homeDirectory", { "/home/adam", NULL, NULL, NULL, NULL } },
-    { "loginShell", { "/bin/bash", NULL, NULL, NULL, NULL } },
-    { "gecos", { "adam", NULL, NULL, NULL, NULL } },
-    { "userPassword", { "{crypt}x", NULL, NULL, NULL, NULL } },
-    { "shadowLastChange", { "0", NULL, NULL, NULL, NULL } },
-    { "shadowMax", { "0", NULL, NULL, NULL, NULL } },
-    { "shadowWarning", { "0", NULL, NULL, NULL, NULL } }
-};
-const int USER_ATTRIBUTES_SIZE = number_of_elements(LDAP_TEST_USER_ATTRIBUTES);
-
 const int CONNECTION_UPDATE_INTERVAL = 1000;
 
-typedef struct context_t
-{
-    struct ldap_global_context_t global_ctx;
-    struct ldap_connection_ctx_t connection_ctx;
-    struct ldap_connection_config_t config;
-} context_t;
-
-static struct context_t* create_context()
-{
-    struct context_t* ctx = malloc(sizeof(context_t));
-    assert_that(ctx, is_non_null);
-
-    ctx->global_ctx.global_ldap = NULL;
-    ctx->global_ctx.talloc_ctx = talloc_new(NULL);
-    assert_that(ctx->global_ctx.talloc_ctx, is_non_null);
-
-    memset(&ctx->connection_ctx, 0, sizeof(ldap_connection_ctx_t));
-
-    char *envvar = "LDAP_SERVER";
-    char *server = get_environment_variable(ctx->global_ctx.talloc_ctx, envvar);
-
-    ctx->config.server = server;
-    ctx->config.port = 389;
-    ctx->config.protocol_verion = LDAP_VERSION3;
-
-    ctx->config.use_sasl = false;
-    ctx->config.use_start_tls = false;
-    ctx->config.chase_referrals = false;
-
-    return ctx;
-}
-
-static void destroy_context(struct context_t* ctx)
-{
-    if (ctx->connection_ctx.ldap_defaults)
-    {
-        if (ctx->connection_ctx.ldap_defaults->authcid)
-        {
-            ldap_memfree(ctx->connection_ctx.ldap_defaults->authcid);
-        }
-
-        if (ctx->connection_ctx.ldap_defaults->authzid)
-        {
-            ldap_memfree(ctx->connection_ctx.ldap_defaults->authzid);
-        }
-
-        if (ctx->connection_ctx.ldap_defaults->realm)
-        {
-            ldap_memfree(ctx->connection_ctx.ldap_defaults->realm);
-        }
-    }
-
-    connection_close(&ctx->connection_ctx);
-    talloc_free(ctx->global_ctx.talloc_ctx);
-    free(ctx);
-}
-
-static void connection_on_delete_message(verto_ctx *ctx, verto_ev *ev)
+static void connection_on_add_message(verto_ctx *ctx, verto_ev *ev)
 {
     (void)(ev);
 
@@ -119,15 +35,15 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
 
     struct ldap_connection_ctx_t* connection = verto_get_private(ev);
 
-    csm_next_state(connection->state_machine);
-
     if (connection->state_machine->state == LDAP_CONNECTION_STATE_RUN)
     {
         verto_del(ev);
 
-        delete(connection, "cn=adam2,ou=users,dc=domain,dc=alt");
+        enum OperationReturnCode rc = delete(connection, "cn=adam2,ou=users,dc=domain,dc=alt");
 
-        verto_add_timeout(ctx, VERTO_EV_FLAG_PERSIST, connection_on_delete_message, CONNECTION_UPDATE_INTERVAL);
+        assert_that(rc, is_equal_to(RETURN_CODE_SUCCESS));
+
+        ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
     }
 
     if (connection->state_machine->state == LDAP_CONNECTION_STATE_ERROR)
@@ -138,45 +54,41 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
     }
 }
 
-Ensure(Cgreen, entry_delete_test) {
-    struct context_t* ctx = create_context();
+static enum OperationReturnCode connection_on_error(int rc, void* unused_a, void* connection)
+{
+    (void)(unused_a);
 
-    ctx->config.use_sasl = true;
+    assert_that(rc, is_not_equal_to(LDAP_SUCCESS));
 
-    ctx->config.sasl_options = talloc(ctx->global_ctx.talloc_ctx, struct ldap_sasl_options_t);
-    ctx->config.sasl_options->mechanism = LDAP_SASL_SIMPLE;
-    ctx->config.sasl_options->passwd = "password";
+    verto_break(((ldap_connection_ctx_t*)connection)->base);
 
-    ctx->config.sasl_options->sasl_nocanon = true;
-    ctx->config.sasl_options->sasl_secprops = "maxssf=56";
-    ctx->config.sasl_options->sasl_flags = LDAP_SASL_QUIET;
-    ctx->connection_ctx.ldap_params = talloc(ctx->global_ctx.talloc_ctx, struct ldap_sasl_params_t);
-    ctx->connection_ctx.ldap_params->dn = "cn=admin,dc=domain,dc=alt";
-    ctx->connection_ctx.ldap_params->passwd = talloc(ctx->global_ctx.talloc_ctx, struct berval);
-    ctx->connection_ctx.ldap_params->passwd->bv_len = strlen(ctx->config.sasl_options->passwd);
-    ctx->connection_ctx.ldap_params->passwd->bv_val = strdup(ctx->config.sasl_options->passwd);
-    ctx->connection_ctx.ldap_params->clientctrls = NULL;
-    ctx->connection_ctx.ldap_params->serverctrls = NULL;
+    fail_test("Entry deletion was not successful\n");
 
-    int rc = RETURN_CODE_FAILURE;
+    return RETURN_CODE_SUCCESS;
+}
 
-    int debug_level = LDAP_DEBUG_ANY;
-    ldap_set_option(ctx->connection_ctx.ldap, LDAP_OPT_DEBUG_LEVEL, &debug_level);
+Ensure(Cgreen, entry_delete_test)
+{
+    TALLOC_CTX* talloc_ctx = talloc_new(NULL);
 
-    rc = connection_configure(&ctx->global_ctx, &ctx->connection_ctx, &ctx->config);
-    assert_that(rc, is_equal_to(RETURN_CODE_SUCCESS));
+    char *envvar = "LDAP_SERVER";
+    char *server = get_environment_variable(talloc_ctx, envvar);
 
-    verto_ev* ev = verto_add_timeout(ctx->connection_ctx.base, VERTO_EV_FLAG_PERSIST, connection_on_timeout,
-                                     CONNECTION_UPDATE_INTERVAL);
-    verto_set_private(ev, &ctx->connection_ctx, NULL);
+    config_t *config = ld_create_config(server, 0, LDAP_VERSION3, "dc=domain,dc=alt",
+                                        "admin", "password", true, false, true, false, CONNECTION_UPDATE_INTERVAL,
+                                        "", "", "");
+    LDHandle *handle = NULL;
+    ld_init(&handle, config);
 
-    verto_run(ctx->connection_ctx.base);
+    ld_install_default_handlers(handle);
+    ld_install_handler(handle, connection_on_timeout, CONNECTION_UPDATE_INTERVAL);
+    ld_install_error_handler(handle, connection_on_error);
 
-    assert_that(ctx->connection_ctx.ldap_defaults, is_not_null);
+    ld_exec(handle);
 
-    talloc_free(ctx->config.sasl_options);
+    ld_free(handle);
 
-    destroy_context(ctx);
+    talloc_free(talloc_ctx);
 }
 
 int main(int argc, char **argv) {
