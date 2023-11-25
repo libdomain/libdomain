7fb9726484813640e0c57cba8ee6bfd1893bed93
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-26 02:09:37 +0400

    fix: add correct name test to test suite in rename_group test

--- tests/auto/group/rename_group/rename_group.c
+++ tests/auto/group/rename_group/rename_group.c
@@ -137,6 +137,6 @@ int main(int argc, char **argv) {
     (void)(argv);
     (void)(contextForCgreen);
     TestSuite *suite = create_test_suite();
-    add_test_with_context(suite, Cgreen, group_add_test);
+    add_test_with_context(suite, Cgreen, group_rename_test);
     return run_test_suite(suite, create_text_reporter());
 }
