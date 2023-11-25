d1e1086b1d0abebe4e97fbc42b9324a35c8b4414
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-26 00:27:04 +0400

    chore: refactor add_group test

--- tests/auto/group/add_group/add_group.c
+++ tests/auto/group/add_group/add_group.c
@@ -1,5 +1,6 @@
 #include <cgreen/cgreen.h>
 
+#include <directory.h>
 #include <domain.h>
 #include <group.h>
 #include <talloc.h>
@@ -15,10 +16,40 @@ Describe(Cgreen);
 BeforeEach(Cgreen) {}
 AfterEach(Cgreen) {}
 
+typedef struct testcase_s
+{
+    char* name;
+    char* entry_parent;
+    char* entry_cn;
+    int number_of_attributes;
+    int desired_test_result;
+    LDAPAttribute_t* attributes;
+} testcase_t;
+
+typedef struct current_testcases_s
+{
+    int number_of_testcases;
+    testcase_t* testcases;
+} current_testcases_t;
+
 #define number_of_elements(x)  (sizeof(x) / sizeof((x)[0]))
 
+static char* OPENLDAP_GROUP_OBJECTCLASS[] = { "top", "posixGroup", NULL };
+static char* OPENLDAP_GROUP_CN[] = { "test_group", NULL };
+static char* OPENLDAP_GROUP_DESCRIPTION[] = { "description", NULL };
+static char* OPENLDAP_GROUP_GID_NUMBER[] = { "100", NULL };
+
+static LDAPAttribute_t OPENLDAP_GROUP_ATTRIBUTES[] =
+{
+    { .name = "objectClass", .values = OPENLDAP_GROUP_OBJECTCLASS },
+    { .name = "cn", .values = OPENLDAP_GROUP_CN },
+    { .name = "description", .values = OPENLDAP_GROUP_DESCRIPTION },
+    { .name = "gidNumber", .values = OPENLDAP_GROUP_GID_NUMBER }
+};
+static const int OPENLDAP_GROUP_ATTRIBUTES_SIZE = number_of_elements(OPENLDAP_GROUP_ATTRIBUTES);
+
 static char* AD_GROUP_OBJECTCLASS[] = { "top", "group", "posixGroup", NULL };
-static char* AD_GROUP_CN[] = { "TestGroup", NULL };
+static char* AD_GROUP_CN[] = { "test_group", NULL };
 static char* AD_GROUP_GOVERNSID[] = { "1.2.840.113556.1.5.8", NULL };
 static char* AD_GROUP_OBJECTCLASSCATEGORY[] = { "1", NULL };
 static char* AD_GROUP_RDNATTID[] = { "cn", NULL };
@@ -90,7 +121,57 @@ static LDAPAttribute_t AD_GROUP_ATTRIBUTES[] =
 
 static const int AD_GROUP_ATTRIBUTES_SIZE = number_of_elements(AD_GROUP_ATTRIBUTES);
 
+static testcase_t OPENLDAP_TESTCASES[] =
+{
+    {
+        "Addition of valid OpenLDAP group testcase",
+        "dc=domain,dc=alt",
+        "test_group",
+        OPENLDAP_GROUP_ATTRIBUTES_SIZE,
+        RETURN_CODE_SUCCESS,
+        OPENLDAP_GROUP_ATTRIBUTES
+    }
+};
+
+static const int NUMBER_OF_OPENLDAP_TESTCASES = number_of_elements(OPENLDAP_TESTCASES);
+
+static testcase_t AD_TESTCASES[] =
+{
+    {
+        "Addition of valid AD group testcase",
+        "dc=domain,dc=alt",
+        "test_group",
+        AD_GROUP_ATTRIBUTES_SIZE,
+        RETURN_CODE_SUCCESS,
+        AD_GROUP_ATTRIBUTES
+    }
+};
+
+static const int NUMBER_OF_AD_TESTCASES = number_of_elements(AD_TESTCASES);
+
 const int CONNECTION_UPDATE_INTERVAL = 1000;
+
+static int current_directory_type = LDAP_TYPE_UNKNOWN;
+
+static current_testcases_t get_current_testcases(int directory_type)
+{
+    current_testcases_t result = { .testcases = NULL, .number_of_testcases = 0 };
+
+    switch (directory_type)
+    {
+    case LDAP_TYPE_ACTIVE_DIRECTORY:
+        result.testcases = AD_TESTCASES;
+        result.number_of_testcases = NUMBER_OF_AD_TESTCASES;
+        break;
+    case LDAP_TYPE_OPENLDAP:
+        result.testcases = OPENLDAP_TESTCASES;
+        result.number_of_testcases = NUMBER_OF_OPENLDAP_TESTCASES;
+    default:
+        break;
+    }
+
+    return result;
+}
 
 static void connection_on_add_message(verto_ctx *ctx, verto_ev *ev)
 {
@@ -114,17 +195,24 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
     {
         verto_del(ev);
 
-        int rc = ld_add_group(connection->handle, "test_group", "description", 100, "dc=domain,dc=alt");
-        if (rc != RETURN_CODE_SUCCESS)
+        current_testcases_t current_testcases = get_current_testcases(current_directory_type);
+        for (int test_index = 0; test_index < current_testcases.number_of_testcases; test_index++)
         {
-            verto_break(ctx);
+            testcase_t testcase = current_testcases.testcases[test_index];
 
-            fail_test("Error encountered during bind\n");
+            int rc = ld_add_group(connection->handle, testcase.entry_cn, &testcase.attributes, testcase.entry_parent);
+            test_status(testcase);
+
+            if (rc != RETURN_CODE_SUCCESS)
+            {
+                verto_break(ctx);
+
+                fail_test("Error encountered ld_add_group bind\n");
+                exit(EXIT_FAILURE);
+            }
         }
-        else
-        {
-            ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
-        }
+
+        ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
     }
 
     if (connection->state_machine->state == LDAP_CONNECTION_STATE_ERROR)
@@ -135,41 +223,9 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
     }
 }
 
-static enum OperationReturnCode connection_on_error(int rc, void* unused_a, void* connection)
-{
-    (void)(unused_a);
-
-    assert_that(rc, is_not_equal_to(LDAP_SUCCESS));
-
-    verto_break(((ldap_connection_ctx_t*)connection)->base);
-
-    fail_test("Group addition was not successful\n");
-
-    return RETURN_CODE_SUCCESS;
-}
-
 Ensure(Cgreen, group_add_test)
 {
-    TALLOC_CTX* talloc_ctx = talloc_new(NULL);
-
-    char *envvar = "LDAP_SERVER";
-    char *server = get_environment_variable(talloc_ctx, envvar);
-
-    config_t *config = ld_create_config(server, 0, LDAP_VERSION3, "dc=domain,dc=alt",
-                                        "admin", "password", true, false, true, false, CONNECTION_UPDATE_INTERVAL,
-                                        "", "", "");
-    LDHandle *handle = NULL;
-    ld_init(&handle, config);
-
-    ld_install_default_handlers(handle);
-    ld_install_handler(handle, connection_on_timeout, CONNECTION_UPDATE_INTERVAL);
-    ld_install_error_handler(handle, connection_on_error);
-
-    ld_exec(handle);
-
-    ld_free(handle);
-
-    talloc_free(talloc_ctx);
+    start_test(connection_on_timeout, CONNECTION_UPDATE_INTERVAL, &current_directory_type);
 }
 
 int main(int argc, char **argv) {
