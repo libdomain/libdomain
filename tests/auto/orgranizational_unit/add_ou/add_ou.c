a8f41c0a1b7ef2805ebd29f7ff6ca7ee4a3de38c
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-26 20:18:33 +0400

    fix: fix test add_ou

--- tests/auto/orgranizational_unit/add_ou/add_ou.c
+++ tests/auto/orgranizational_unit/add_ou/add_ou.c
@@ -198,9 +198,16 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
         {
             testcase_t testcase = current_testcases.testcases[test_index];
 
-            enum OperationReturnCode rc = ld_add_ou(connection->handle, testcase.entry_cn, &testcase.attributes, testcase.parent_dn);
+            TALLOC_CTX* talloc_ctx = talloc_new(NULL);
+
+            enum OperationReturnCode rc = ld_add_ou(connection->handle,
+                                                    testcase.entry_cn,
+                                                    fill_user_attributes(talloc_ctx, testcase.attributes, testcase.number_of_attributes),
+                                                    testcase.parent_dn);
             assert_that(rc,is_equal_to(testcase.desired_test_result));
             test_status(testcase);
+
+            talloc_free(talloc_ctx);
         }
 
         ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
