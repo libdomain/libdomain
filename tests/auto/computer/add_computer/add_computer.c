af1ae361769ad7bf8ab5ea1f1fb32dbb832658ed
august-alt <77973983+august-alt@users.noreply.github.com>
2023-11-26 16:45:06 +0400

    feat: add support of ad to add_computer test

--- tests/auto/computer/add_computer/add_computer.c
+++ tests/auto/computer/add_computer/add_computer.c
@@ -1,5 +1,6 @@
 #include <cgreen/cgreen.h>
 
+#include <directory.h>
 #include <domain.h>
 #include <computer.h>
 #include <talloc.h>
@@ -118,6 +119,74 @@ static LDAPAttribute_t AD_COMPUTER_ATTRIBUTES[] =
 
 static const int AD_COMPUTER_ATTRIBUTES_SIZE = number_of_elements(AD_COMPUTER_ATTRIBUTES);
 
+#define number_of_elements(x)  (sizeof(x) / sizeof((x)[0]))
+
+typedef struct testcase_s
+{
+    char* name;
+    char* entry_cn;
+    char* entry_parent;
+    int number_of_attributes;
+    int desired_test_result;
+    LDAPAttribute_t* attributes;
+} testcase_t;
+
+typedef struct current_testcases_s
+{
+    int number_of_testcases;
+    testcase_t* testcases;
+} current_testcases_t;
+
+static testcase_t OPENLDAP_TESTCASES[] =
+{
+    {
+        "Test computer creation OpenLDAP",
+        "test_computer_add",
+        "ou=equipment,dc=domain,dc=alt",
+        OPENLDAP_COMPUTER_ATTRIBUTES_SIZE,
+        RETURN_CODE_SUCCESS,
+        OPENLDAP_COMPUTER_ATTRIBUTES
+    }
+};
+
+static const int NUMBER_OF_OPENLDAP_TESTCASES = number_of_elements(OPENLDAP_TESTCASES);
+
+static testcase_t AD_TESTCASES[] =
+{
+    {
+        "Test computer creation AD",
+        "test_computer_add",
+        "dc=domain,dc=alt",
+        AD_COMPUTER_ATTRIBUTES_SIZE,
+        RETURN_CODE_SUCCESS,
+        AD_COMPUTER_ATTRIBUTES
+    }
+};
+
+static const int NUMBER_OF_AD_TESTCASES = number_of_elements(AD_TESTCASES);
+
+static int current_directory_type = LDAP_TYPE_UNKNOWN;
+
+static current_testcases_t get_current_testcases(int directory_type)
+{
+    current_testcases_t result = { .testcases = NULL, .number_of_testcases = 0 };
+
+    switch (directory_type)
+    {
+    case LDAP_TYPE_ACTIVE_DIRECTORY:
+        result.testcases = AD_TESTCASES;
+        result.number_of_testcases = NUMBER_OF_AD_TESTCASES;
+        break;
+    case LDAP_TYPE_OPENLDAP:
+        result.testcases = OPENLDAP_TESTCASES;
+        result.number_of_testcases = NUMBER_OF_OPENLDAP_TESTCASES;
+    default:
+        break;
+    }
+
+    return result;
+}
+
 const int CONNECTION_UPDATE_INTERVAL = 1000;
 
 static void connection_on_add_message(verto_ctx *ctx, verto_ev *ev)
@@ -142,26 +211,27 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
     {
         verto_del(ev);
 
-        int rc = ld_add_computer(connection->handle,
-                                 "test_computer_add",
-                                 "description",
-                                 "11-77-23-15",
-                                 "dc=domain,dc=alt",
-                                 "test org. inc",
-                                 "room 12",
-                                 "cn=john smith,ou=people,dc=domain,dc=alt",
-                                 "r&d",
-                                 "ou=equipment,dc=domain,dc=alt");
-        if (rc != RETURN_CODE_SUCCESS)
+        current_testcases_t current_testcases = get_current_testcases(current_directory_type);
+        for (int test_index = 0; test_index < current_testcases.number_of_testcases; test_index++)
         {
-            verto_break(ctx);
+            testcase_t testcase = current_testcases.testcases[test_index];
 
-            fail_test("Error encountered during bind\n");
+            enum OperationReturnCode rc = ld_add_computer(connection->handle,
+                                                          testcase.entry_cn,
+                                                          &testcase.attributes,
+                                                          testcase.entry_parent);
+            assert_that(rc,is_equal_to(testcase.desired_test_result));
+            test_status(testcase);
+            if (rc != RETURN_CODE_SUCCESS)
+            {
+                verto_break(ctx);
+
+                fail_test("Error encountered during computer creation!\n");
+                break;
+            }
         }
-        else
-        {
-            ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
-        }
+
+        ld_install_handler(connection->handle, connection_on_add_message, CONNECTION_UPDATE_INTERVAL);
     }
 
     if (connection->state_machine->state == LDAP_CONNECTION_STATE_ERROR)
@@ -172,41 +242,9 @@ static void connection_on_timeout(verto_ctx *ctx, verto_ev *ev)
     }
 }
 
-static enum OperationReturnCode connection_on_error(int rc, void* unused_a, void* connection)
-{
-    (void)(unused_a);
-
-    assert_that(rc, is_not_equal_to(LDAP_SUCCESS));
-
-    verto_break(((ldap_connection_ctx_t*)connection)->base);
-
-    fail_test("Computer addition was not successful\n");
-
-    return RETURN_CODE_SUCCESS;
-}
-
 Ensure(Cgreen, computer_add_test)
 {
-    TALLOC_CTX* talloc_ctx = talloc_new(NULL);
-
-    char *envvar = "LDAP_SERVER";
-    char *server = get_environment_variable(talloc_ctx, envvar);
-
-    config_t *config = ld_create_config(server, 0, LDAP_VERSION3, "dc=domain,dc=alt",
-                                        "admin", "password", true, false, true, false, CONNECTION_UPDATE_INTERVAL,
-                                        "", "", "");
-    LDHandle *handle = NULL;
-    ld_init(&handle, config);
-
-    ld_install_default_handlers(handle);
-    ld_install_handler(handle, connection_on_timeout, CONNECTION_UPDATE_INTERVAL);
-    ld_install_error_handler(handle, connection_on_error);
-
-    ld_exec(handle);
-
-    ld_free(handle);
-
-    talloc_free(talloc_ctx);
+    start_test(connection_on_timeout, CONNECTION_UPDATE_INTERVAL, &current_directory_type);
 }
 
 int main(int argc, char **argv) {
