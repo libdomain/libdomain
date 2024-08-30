b635285e55796750e54c83e1cdd4301617f2277c
Kirill Sharov <kira.sharov@yandex.ru>
2024-08-30 11:46:48 +0400

    wip: if construction for test pipeline type is added to make-test.sh

--- tests/testenv-container/make-test.sh
+++ tests/testenv-container/make-test.sh
@@ -8,4 +8,10 @@ cp /krb5.conf /etc/krb5.conf
 cat /resolv.conf > /etc/resolv.conf
 chown -R builder2:builder2 /app/
 echo 'password145Qw!' | kinit administrator@DOMAIN.ALT || :
-cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest --verbose
+cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd 
+
+if [[ "$PIPELINE_TYPE" == "MEMCHECK" ]]; then
+  cmake cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -DCMAKE_BUILD_TYPE=Debug -B . .. && make # && ctest --output-on-failure
+else
+  cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest --verbose
+fi
