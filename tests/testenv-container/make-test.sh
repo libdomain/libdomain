6fccb69d76baad6476aea72019592958f5bad4af
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2024-09-02 16:39:42 +0400

    test: add github action that check memory leaks using valgrind memcheck #72
    * wip: valgrind memcheck workflow is added
    
    * wip: params for valgrind.yml added to main.yml
    
    * wip: cmake flags for tests is added to valgrind.yml
    
    * wip: add make_valgrind script and arg var to Dockerfile of testenv-container
    
    * wip: pipeline type variable is added to test and valgrind workflows
    
    * wip: add env var to dockerfile entrypoint
    
    * wip: if construction for test pipeline type is added to make-test.sh
    
    * wip: comments is deleted
    
    * wip: memcheck option for ctest by valgrind is added to tests/CMakeLists.txt
    
    * wip: make-valgrind.sh is deleted
    
    * wip: conditional ctest calling for valgrind by env var value is added to make-test.sh
    
    * wip: `CTest` including is added to root `CMakeLists`
    
    * wip: local docker-compose file is deleted
    
    * wip: `test` workflow changes is rolled back
    
    * wip: ENV and default value of ARG is added to Dockerfile
    
    * wip: formatting is fixed and env var name is changed to TEST_TYPE
    
    * wip: valgrind test is deleted from main.yml (it in test.yml)
    
    * wip: valgrind.yml is deleted
    
    * wip: ENV and ARG is deleted from Dockerfile
    
    * wip: condition is deleted
    
    * wip: CTest including is removed from `tests/CMakeLists.txt`

--- tests/testenv-container/make-test.sh
+++ tests/testenv-container/make-test.sh
@@ -8,4 +8,11 @@ cp /krb5.conf /etc/krb5.conf
 cat /resolv.conf > /etc/resolv.conf
 chown -R builder2:builder2 /app/
 echo 'password145Qw!' | kinit administrator@DOMAIN.ALT || :
-cd /app/ && rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR && pwd && cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -B . .. && make && ctest --verbose
+cd /app/ && \
+rm -rf $BUILD_DIR && \
+mkdir $BUILD_DIR && \
+cd $BUILD_DIR && \
+pwd && \
+cmake -DLIBDOMAIN_BUILD_TESTS:BOOL=ON -DCMAKE_BUILD_TYPE=Debug -B . .. && \
+make && \
+ctest --overwrite MemoryCheckCommandOptions="-q --leak-check=full --error-exitcode=99" -T memcheck --verbose
