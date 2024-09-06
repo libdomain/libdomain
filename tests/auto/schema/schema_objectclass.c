fa782fe6d5f78acf3943acbde4bf7704e515a6a2
Kirill Sharov <kira.sharov@yandex.ru>
2024-09-06 15:29:15 +0400

    wip: reformated tests for getters

--- tests/auto/schema/schema_objectclass.c
+++ tests/auto/schema/schema_objectclass.c
@@ -55,55 +55,7 @@ Ensure(returns_false_on_null_objectclass_with_valid_schema) {
     talloc_free(ctx);
 }
 
-Ensure(get_objectclass_with_valid_schema) {
-    TALLOC_CTX *ctx = talloc_new(NULL);
-
-    struct ldap_schema_t *schema = ldap_schema_new(ctx);
-
-    LDAPObjectClass** objectclasses = talloc_array(ctx, LDAPObjectClass*, 10);
-
-    for (int i = 0; i < 10; ++i)
-    {
-        objectclasses[i] = talloc_zero(ctx, LDAPObjectClass);
-        objectclasses[i]->oc_names = talloc_array(ctx, char*, 4);
-
-        char* first_name_buffer = malloc(30);
-        sprintf(first_name_buffer, "test_objectclass_first_name_%d", i);
-        objectclasses[i]->oc_names[0] = first_name_buffer;
-
-        char* second_name_buffer = malloc(30);
-        sprintf(second_name_buffer, "test_objectclass_second_name_%d", i);
-        objectclasses[i]->oc_names[1] = second_name_buffer;
-
-        char* third_name_buffer = malloc(30);
-        sprintf(third_name_buffer, "test_objectclass_third_name_%d", i);
-        objectclasses[i]->oc_names[2] = third_name_buffer;
-
-        objectclasses[i]->oc_names[3] = NULL;
-
-        char* oid_buffer = malloc(30);
-        sprintf(oid_buffer, "test_objectclass_oid_%d", i);
-        objectclasses[i]->oc_oid = oid_buffer;
-
-        ldap_schema_append_objectclass(schema, objectclasses[i]);
-    }
-
-    for (int i = 0; i < 10; ++i)
-    {
-        LDAPObjectClass* result = ldap_schema_get_objectclass_by_oid(schema, objectclasses[i]->oc_oid);
-        assert_that(result, is_equal_to(objectclasses[i]));
-
-        for (int j = 0; j < 3; ++j)
-        {
-            result = ldap_schema_get_objectclass_by_name(schema, objectclasses[i]->oc_names[j]);
-            assert_that(result, is_equal_to(objectclasses[i]));
-        }
-    }
-
-    talloc_free(ctx);
-}
-
-Ensure(get_objectclass_with_valid_schema_and_invalid_name_or_oid)
+Ensure(get_objectclass_returns_null_on_valid_schema_and_invalid_name_or_oid)
 {
     TALLOC_CTX *ctx = talloc_new(NULL);
 
@@ -129,7 +81,7 @@ Ensure(get_objectclass_with_valid_schema_and_invalid_name_or_oid)
     talloc_free(ctx);
 }
 
-Ensure(get_objectclass_with_invalid_schema)
+Ensure(get_objectclass_returns_null_on_invalid_schema)
 {
     LDAPObjectClass* result = ldap_schema_get_objectclass_by_name(NULL, "test_objectclass_name");
 
@@ -148,9 +100,8 @@ schema_objectclass_test_suite()
     add_test(suite, returns_true_on_successful_append_of_objectclass);
     add_test(suite, returns_false_on_null_schema_with_valid_objectclass);
     add_test(suite, returns_false_on_null_objectclass_with_valid_schema);
-    add_test(suite, get_objectclass_with_valid_schema);
-    add_test(suite, get_objectclass_with_valid_schema_and_invalid_name_or_oid);
-    add_test(suite, get_objectclass_with_invalid_schema);
+    add_test(suite, get_objectclass_returns_null_on_valid_schema_and_invalid_name_or_oid);
+    add_test(suite, get_objectclass_returns_null_on_invalid_schema);
 
     return suite;
 }
