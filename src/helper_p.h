14e6c3e4d706a735bc40da1842cb9b0c624c33b4
kiper220-alt <gedertkj@basealt.ru>
2024-09-18 16:29:37 +0400

    feat: add error message variant to `ld_talloc`

--- src/helper_p.h
+++ src/helper_p.h
@@ -40,62 +40,119 @@
     } \
     ctx = NULL; 
 
+#define ld_talloc_e(var, error_handler, error_message, ctx, type) \
+    LD_ALLOC_HELPER(var, talloc, error_message, error_handler, ctx, type)
+
 #define ld_talloc(var, error_handler, ctx, type) \
-    LD_ALLOC_HELPER(var, talloc, "Error - failed to allocate memory", error_handler, ctx, type)
+    ld_talloc_e(var, error_handler, "Error - failed to allocate memory", ctx, type)
+
+#define ld_talloc_array_e(var, error_handler, error_message, ctx, type, count) \
+    LD_ALLOC_HELPER(var, talloc_array, error_message, error_handler, ctx, type, count)
 
 #define ld_talloc_array(var, error_handler, ctx, type, count) \
-    LD_ALLOC_HELPER(var, talloc_array, "Error - failed to allocate memory", error_handler, ctx, type, count)
+    ld_talloc_array_e(var, error_handler, "Error - failed to allocate memory", ctx, type, count)
+
+#define ld_talloc_size_e(var, error_handler, error_message, ctx, size) \
+    LD_ALLOC_HELPER(var, talloc_size, error_message, error_handler, ctx, size)
 
 #define ld_talloc_size(var, error_handler, ctx, size) \
-    LD_ALLOC_HELPER(var, talloc_size, "Error - failed to allocate memory", error_handler, ctx, size)
+    ld_talloc_size_e(var, error_handler, "Error - failed to allocate memory", ctx, size)
+
+#define ld_talloc_zero_e(var, error_handler, error_message, ctx, type) \
+    LD_ALLOC_HELPER(var, talloc_zero, error_message, error_handler, ctx, type)
 
 #define ld_talloc_zero(var, error_handler, ctx, type) \
-    LD_ALLOC_HELPER(var, talloc_zero, "Error - failed to allocate memory", error_handler, ctx, type)
+    ld_talloc_zero_e(var, error_handler, "Error - failed to allocate memory", ctx, type)
+
+#define ld_talloc_zero_array_e(var, error_handler, error_message, ctx, type, count) \
+    LD_ALLOC_HELPER(var, talloc_zero_array, error_message, error_handler, ctx, type, count)
 
 #define ld_talloc_zero_array(var, error_handler, ctx, type, count) \
-    LD_ALLOC_HELPER(var, talloc_zero_array, "Error - failed to allocate memory", error_handler, ctx, type, count)
+    ld_talloc_zero_array_e(var, error_handler, "Error - failed to allocate memory", ctx, type, count)
+
+#define ld_talloc_zero_size_e(var, error_handler, error_message, ctx, size) \
+    LD_ALLOC_HELPER(var, talloc_zero_size, error_message, error_handler, ctx, size)
 
 #define ld_talloc_zero_size(var, error_handler, ctx, size) \
-    LD_ALLOC_HELPER(var, talloc_zero_size, "Error - failed to allocate memory", error_handler, ctx, size)
+    ld_talloc_zero_size_e(var, error_handler, "Error - failed to allocate memory", ctx, size)
+
+#define ld_talloc_asprintf_e(var, error_handler, error_message, ctx, fmt, ...) \
+    LD_ALLOC_HELPER(var, talloc_asprintf, error_message, error_handler, ctx, fmt, __VA_ARGS__)
 
 #define ld_talloc_asprintf(var, error_handler, ctx, fmt, ...) \
-    LD_ALLOC_HELPER(var, talloc_asprintf, "Error - failed to allocate memory", error_handler, ctx, fmt, __VA_ARGS__)
+    ld_talloc_asprintf_e(var, error_handler, "Error - failed to allocate memory", ctx, fmt, ...)
+
+#define ld_talloc_asprintf_addbuf_e(var, error_handler, error_message, ps, fmt, ...) \
+    LD_ALLOC_HELPER(var, talloc_asprintf_addbuf, error_message, error_handler, ps, fmt, __VA_ARGS__)
 
 #define ld_talloc_asprintf_addbuf(var, error_handler, ps, fmt, ...) \
-    LD_ALLOC_HELPER(var, talloc_asprintf_addbuf, "Error - failed to allocate memory", error_handler, ps, fmt, __VA_ARGS__)
+    ld_talloc_asprintf_addbuf_e(var, error_handler, "Error - failed to allocate memory", ps, fmt, ...)
+
+#define ld_talloc_asprintf_append_e(var, error_handler, error_message, str, fmt, ...) \
+    LD_ALLOC_HELPER(var, talloc_asprintf_append, error_message, error_handler, str, fmt, __VA_ARGS__)
 
 #define ld_talloc_asprintf_append(var, error_handler, str, fmt, ...) \
-    LD_ALLOC_HELPER(var, talloc_asprintf_append, "Error - failed to allocate memory", error_handler, str, fmt, __VA_ARGS__)
+    ld_talloc_asprintf_append_e(var, error_handler, "Error - failed to allocate memory", str, fmt, ...)
+
+#define ld_talloc_asprintf_append_buffer_e(var, error_handler, error_message, str, fmt, ...) \
+    LD_ALLOC_HELPER(var, talloc_asprintf_append_buffer, error_message, error_handler, str, fmt, __VA_ARGS__)
 
 #define ld_talloc_asprintf_append_buffer(var, error_handler, str, fmt, ...) \
-    LD_ALLOC_HELPER(var, talloc_asprintf_append_buffer, "Error - failed to allocate memory", error_handler, str, fmt, __VA_ARGS__)
+    ld_talloc_asprintf_append_buffer_e(var, error_handler, "Error - failed to allocate memory", str, fmt, ...)
+
+#define ld_talloc_vasprintf_e(var, error_handler, error_message, ctx, fmt, valist) \
+    LD_ALLOC_HELPER(var, talloc_vasprintf, error_message, error_handler, ctx, fmt, valist)
 
 #define ld_talloc_vasprintf(var, error_handler, ctx, fmt, valist) \
-    LD_ALLOC_HELPER(var, talloc_vasprintf, "Error - failed to allocate memory", error_handler, ctx, fmt, valist)
+    ld_talloc_vasprintf_e(var, error_handler, "Error - failed to allocate memory", ctx, fmt, valist)
+
+#define ld_talloc_vasprintf_append_e(var, error_handler, error_message, str, fmt, valist) \
+    LD_ALLOC_HELPER(var, talloc_vasprintf_append, error_message, str, fmt, valist)
 
 #define ld_talloc_vasprintf_append(var, error_handler, str, fmt, valist) \
-    LD_ALLOC_HELPER(var, talloc_vasprintf_append, "Error - failed to allocate memory", str, fmt, valist)
+    ld_talloc_vasprintf_append_e(var, error_handler, "Error - failed to allocate memory", str, fmt, valist)
+
+#define ld_talloc_vasprintf_append_buffer_e(var, error_handler, error_message, fmt, valist) \
+    LD_ALLOC_HELPER(var, talloc_vasprintf_append_buffer, error_message, error_handler, str, fmt, valist)
 
 #define ld_talloc_vasprintf_append_buffer(var, error_handler, fmt, valist) \
-    LD_ALLOC_HELPER(var, talloc_vasprintf_append_buffer, "Error - failed to allocate memory", error_handler, str, fmt, valist)
+    ld_talloc_vasprintf_append_buffer_e(var, error_handler, "Error - failed to allocate memory", fmt, valist)
+
+#define ld_talloc_new_e(var, error_handler, error_message, ctx) \
+    LD_ALLOC_HELPER(var, talloc_new, error_message, error_handler, ctx)
 
 #define ld_talloc_new(var, error_handler, ctx) \
-    LD_ALLOC_HELPER(var, talloc_new, "Error - failed to allocate memory", error_handler, ctx)
+    ld_talloc_new_e(var, error_handler, "Error - failed to allocate memory", ctx)
+
+#define ld_talloc_memdup_e(var, error_handler, error_message, t, p, size) \
+    LD_ALLOC_HELPER(var, talloc_memdup, error_message, error_handler, t, p, size)
 
 #define ld_talloc_memdup(var, error_handler, t, p, size) \
-    LD_ALLOC_HELPER(var, talloc_memdup, "Error - failed to allocate memory", error_handler, t, p, size)
+    ld_talloc_memdup_e(var, error_handler, "Error - failed to allocate memory", t, p, size)
+
+#define ld_talloc_strdup_e(var, error_handler, error_message, ctx, str) \
+    LD_ALLOC_HELPER(var, talloc_strdup, error_message, error_handler, ctx, str)
 
 #define ld_talloc_strdup(var, error_handler, ctx, str) \
-    LD_ALLOC_HELPER(var, talloc_strdup, "Error - failed to allocate memory", error_handler, ctx, str)
+    ld_talloc_strdup_e(var, error_handler, "Error - failed to allocate memory", ctx, str)
+
+#define ld_talloc_strndup_e(var, error_handler, error_message, ctx, str, n) \
+    LD_ALLOC_HELPER(var, talloc_strndup, error_message, error_handler, ctx, str, n)
 
 #define ld_talloc_strndup(var, error_handler, ctx, str, n) \
-    LD_ALLOC_HELPER(var, talloc_strndup, "Error - failed to allocate memory", error_handler, ctx, str, n)
+    ld_talloc_strndup_e(var, error_handler, "Error - failed to allocate memory", ctx, str, n)
+
+#define ld_talloc_realloc_e(var, error_handler, error_message, ctx, type, count) \
+    LD_ALLOC_HELPER(var, talloc_realloc, error_message, error_handler, ctx, var, type, count)
 
 #define ld_talloc_realloc(var, error_handler, ctx, type, count) \
-    LD_ALLOC_HELPER(var, talloc_realloc, "Error - failed to allocate memory", error_handler, ctx, var, type, count)
+    ld_talloc_realloc_e(var, error_handler, "Error - failed to allocate memory", ctx, type, count)
+
+#define ld_talloc_free_e(ctx, error_handler, error_message) \
+    LD_FREE_HELPER(talloc_free, error_message, error_handler, ctx)
 
 #define ld_talloc_free(ctx, error_handler) \
-    LD_FREE_HELPER(talloc_free, "Error - failed to free memory", error_handler, ctx)
+ld_talloc_free_e(ctx, error_handler, "Error - failed to free memory")
 
 
 
